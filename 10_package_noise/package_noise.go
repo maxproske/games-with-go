d58b1f90942169a138e7dd5c3ac09c9e33291938
Max Proske <mhcproske@gmail.com>
2018-04-23 22:06:34 -0700

    Fix race condition, add channels

--- 10_package_noise/package_noise.go
+++ 10_package_noise/package_noise.go
@@ -1,6 +1,7 @@
 package noise
 
 import (
+	"math"
 	"runtime"
 	"sync"
 )
@@ -19,15 +20,18 @@ const (
 func MakeNoise(noiseType NoiseType, frequency, lacunarity, gain float32, octaves, w, h int) (noise []float32, min, max float32) {
 	// Because we declare these in our return, we don't need to declare, only initialize
 	noise = make([]float32, w*h)
-	min = float32(9990.0)
-	max = float32(-9999.0)
+	innerMin := float32(math.MaxFloat32)
+	innerMax := float32(-math.MaxFloat32)
 
 	// Set num goroutines
 	numRoutines := runtime.NumCPU()
 	batchSize := len(noise) / numRoutines
 
+	// Channels, list goroutines can put things and pass to others
+	minMaxChan := make(chan float32, numRoutines*2) // min and max in 1 channel (so *2)
+
 	// Make waitgroup
-	var mutex = &sync.Mutex{}
+	//var mutex = &sync.Mutex{}
 	var wg sync.WaitGroup
 	wg.Add(numRoutines)
 
@@ -47,19 +51,42 @@ func MakeNoise(noiseType NoiseType, frequency, lacunarity, gain float32, octaves
 					noise[j] = Fbm2(float32(x), float32(y), frequency, lacunarity, gain, octaves)
 				}
 
-				if noise[j] < min || noise[j] > max {
-					mutex.Lock()
-					if noise[j] < min {
-						min = noise[j]
-					} else if noise[j] > max {
-						max = noise[j]
-					}
-					mutex.Unlock()
+				// Check if we have noise below the min or above the max
+				if noise[j] < innerMin {
+					innerMin = noise[j]
+				} else if noise[j] > innerMax {
+					innerMax = noise[j]
 				}
 			}
+
+			// Put values in channel
+			minMaxChan <- innerMin
+			minMaxChan <- innerMax
+
+			// Only lock once per goroutine, not milliosn of times inside the loop.
+			// Check if inner min is less than min
+			// mutex.Lock()
+			// if innerMin < min {
+			// 	min = innerMin
+			// } else if innerMax > max {
+			// 	max = innerMax
+			// }
+			// mutex.Unlock()
 		}(i)
 	}
 	wg.Wait()
+
+	// Close our channel
+	close(minMaxChan)
+	// Now iterate over it (same thing as mutexes)
+	for v := range minMaxChan {
+		if v < min {
+			min = v
+		} else if v > max {
+			max = v
+		}
+	}
+
 	return noise, min, max
 }
 
