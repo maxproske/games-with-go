3eb929ade71717eafe8fe34f4c665d5b3fb089e1
Max Proske <mhcproske@gmail.com>
2018-06-02 12:48:37 -0700

    Finish lesson on parsing

---
+++ 21_parsing/apt/apt.go
@@ -0,0 +1,498 @@
+package apt
+
+import (
+	"math"
+	"math/rand"
+	"reflect"
+	"strconv"
+
+	"github.com/PrawnSkunk/games-with-go/10_package_noise"
+)
+
+//////////////////////
+// Node
+//////////////////////
+
+// An interface is a contract that lets you define a set of functions
+type Node interface {
+	// Provide an eval function. When given a node of a tree (+), it will compute the full result
+	Eval(x, y float32) float32
+	String() string
+	SetParent(parent Node)
+	SetChildren([]Node)
+	GetParent() Node
+	GetChildren() []Node // We don't have the ability to access .Children
+	AddRandom(node Node) // add to a random location in the tree
+	AddLeaf(leaf Node) bool
+	NodeCount() int
+}
+
+func Mutate(node Node) Node {
+	// Get a random node to mutate into (can be regular or leaf)
+	r := rand.Intn(12)
+	var mutatedNode Node
+	if r <= 9 {
+		mutatedNode = GetRandomNode()
+	} else {
+		mutatedNode = GetRandomLeaf()
+	}
+
+	// Check if root of the tree
+	if node.GetParent() != nil {
+		// Adjust the parent's child to point to the mutated node
+		for i, parentChild := range node.GetParent().GetChildren() {
+			if parentChild == node {
+				// We found the spot in the parent's children array to modify
+				node.GetParent().GetChildren()[i] = mutatedNode
+			}
+		}
+	}
+
+	// Take the children for the old node, and put them in the new node
+	for i, child := range node.GetChildren() {
+		if i >= len(mutatedNode.GetChildren()) {
+			break // If the new node doesn't have as many as the old node, break out
+		}
+		mutatedNode.GetChildren()[i] = child // If we do have space, set new node to spot of old node
+		child.SetParent(mutatedNode)         // Set its parent
+	}
+
+	for i, child := range mutatedNode.GetChildren() {
+		// It's possible the new tree hasn't got all nil pointers filled up
+		if child == nil {
+			leaf := GetRandomLeaf()
+			leaf.SetParent(mutatedNode)
+			mutatedNode.GetChildren()[i] = leaf
+		}
+	}
+
+	mutatedNode.SetParent(node.GetParent())
+	return mutatedNode
+}
+
+func GetRandomNode() Node {
+	// Non-leaf nodes only
+	r := rand.Intn(9)
+	switch r {
+	case 0:
+		return NewOpPlus()
+	case 1:
+		return NewOpMinus()
+	case 2:
+		return NewOpMult()
+	case 3:
+		return NewOpDiv()
+	case 4:
+		return NewOpAtan2()
+	case 5:
+		return NewOpAtan()
+	case 6:
+		return NewOpCos()
+	case 7:
+		return NewOpSin()
+	case 8:
+		return NewOpNoise()
+	}
+	panic("Get Random Node Failed")
+}
+
+func GetRandomLeaf() Node {
+	r := rand.Intn(3)
+	switch r {
+	case 0:
+		return NewOpX()
+	case 1:
+		return NewOpY()
+	case 2:
+		return NewOpConstant() // Between 0-1
+	}
+	panic("Get Random Leaf Failed")
+}
+
+//////////////////////
+// BaseNode
+//////////////////////
+type BaseNode struct {
+	Parent   Node
+	Children []Node
+}
+
+func (node *BaseNode) AddRandom(nodeToAdd Node) {
+	// Sin gets mutated to +. Now needs another branch!
+	addIndex := rand.Intn(len(node.Children)) // Select which child branch we wnat to add a new node to
+	if node.Children[addIndex] == nil {
+		nodeToAdd.SetParent(node) // Set parent
+		node.Children[addIndex] = nodeToAdd
+	} else {
+		node.Children[addIndex].AddRandom(nodeToAdd) // pass it down the tree
+	}
+}
+
+func (node *BaseNode) SetParent(parent Node) {
+	node.Parent = parent
+}
+
+func (node *BaseNode) SetChildren(children []Node) {
+	node.Children = children
+}
+
+func (node *BaseNode) AddLeaf(leaf Node) bool {
+	// Look for the nil children, and fill them in with leafs
+	for i, child := range node.Children {
+		if child == nil {
+			leaf.SetParent(node)
+			node.Children[i] = leaf
+			return true
+		} else if node.Children[i].AddLeaf(leaf) {
+			return true
+		}
+	}
+	return false // Unable to find a leaf to add to
+}
+
+// BaseNode will not be a Node until we satisfy the interface
+func (node *BaseNode) Eval(x, y float32) float32 {
+	panic("Tried to call eval on basenode")
+}
+
+func (node *BaseNode) String() string {
+	panic("Tried to call string on basenode")
+}
+
+func (node *BaseNode) NodeCount() int {
+	count := 1 // The node we are at right now
+	for _, child := range node.Children {
+		count += child.NodeCount()
+	}
+	return count
+}
+
+func (node *BaseNode) GetChildren() []Node {
+	return node.Children
+}
+
+func (node *BaseNode) GetParent() Node {
+	return node.Parent
+}
+
+//////////////////////
+// OpPicture
+//////////////////////
+type OpPicture struct {
+	BaseNode
+}
+
+// Constructor
+func NewOpPicture() *OpPicture {
+	return &OpPicture{BaseNode{nil, make([]Node, 3)}}
+}
+
+func (op *OpPicture) Eval(x, y float32) float32 {
+	panic("eval called on root of picture tree")
+}
+
+func (op *OpPicture) String() string {
+	return "( Picture \n" + op.Children[0].String() + "\n" + op.Children[1].String() + "\n" + op.Children[2].String() + " )"
+}
+
+//////////////////////
+// OpX
+//////////////////////
+
+// Not equivalent to "type OpX BaseNode", because it doesn't inherit BaseNode
+type OpX struct {
+	BaseNode
+}
+
+// No built-in structure for constructors. But there is a convention:
+func NewOpX() *OpX {
+	return &OpX{BaseNode{nil, make([]Node, 0)}}
+}
+
+func (op *OpX) Eval(x, y float32) float32 {
+	return x // base case when stopping recursion
+}
+
+func (op *OpX) String() string {
+	return "X"
+}
+
+//////////////////////
+// OpY
+//////////////////////
+
+type OpY struct {
+	BaseNode
+}
+
+func NewOpY() *OpY {
+	return &OpY{BaseNode{nil, make([]Node, 0)}}
+}
+
+func (op *OpY) Eval(x, y float32) float32 {
+	return y
+}
+
+func (op *OpY) String() string {
+	return "Y"
+}
+
+//////////////////////
+// OpConstant
+//////////////////////
+
+type OpConstant struct {
+	BaseNode
+	value float32
+}
+
+func NewOpConstant() *OpConstant {
+	return &OpConstant{BaseNode{nil, make([]Node, 0)}, rand.Float32()*2 - 1}
+}
+
+func (op *OpConstant) Eval(x, y float32) float32 {
+	return op.value
+}
+
+func (op *OpConstant) String() string {
+	return strconv.FormatFloat(float64(op.value), 'f', 9, 32) // Converts floating point to a string
+}
+
+//////////////////////
+// OpPlus
+//////////////////////
+
+type OpPlus struct {
+	// Struct embedding. Instead of giving it a LeftChild and RightChild node, say it has a BaseNode
+	BaseNode
+}
+
+func NewOpPlus() *OpPlus {
+	return &OpPlus{BaseNode{nil, make([]Node, 2)}}
+}
+
+// OpPlus.Eval(...) has the same signature as the Node interface
+func (op *OpPlus) Eval(x, y float32) float32 {
+	return op.Children[0].Eval(x, y) + op.Children[1].Eval(x, y)
+}
+
+func (op *OpPlus) String() string {
+	return "( + " + op.Children[0].String() + " " + op.Children[1].String() + " )"
+}
+
+//////////////////////
+// OpMinus
+//////////////////////
+
+type OpMinus struct {
+	BaseNode
+}
+
+func NewOpMinus() *OpMinus {
+	return &OpMinus{BaseNode{nil, make([]Node, 2)}}
+}
+
+func (op *OpMinus) Eval(x, y float32) float32 {
+	return op.Children[0].Eval(x, y) - op.Children[1].Eval(x, y)
+}
+
+func (op *OpMinus) String() string {
+	return "( - " + op.Children[0].String() + " " + op.Children[1].String() + " )"
+}
+
+//////////////////////
+// OpMult
+//////////////////////
+
+type OpMult struct {
+	BaseNode
+}
+
+func NewOpMult() *OpMult {
+	return &OpMult{BaseNode{nil, make([]Node, 2)}}
+}
+
+func (op *OpMult) Eval(x, y float32) float32 {
+	return op.Children[0].Eval(x, y) * op.Children[1].Eval(x, y)
+}
+
+func (op *OpMult) String() string {
+	return "( * " + op.Children[0].String() + " " + op.Children[1].String() + " )"
+}
+
+//////////////////////
+// OpDiv
+//////////////////////
+
+type OpDiv struct {
+	BaseNode
+}
+
+func NewOpDiv() *OpDiv {
+	return &OpDiv{BaseNode{nil, make([]Node, 2)}}
+}
+
+func (op *OpDiv) Eval(x, y float32) float32 {
+	return op.Children[0].Eval(x, y) / op.Children[1].Eval(x, y)
+}
+
+func (op *OpDiv) String() string {
+	return "( / " + op.Children[0].String() + " " + op.Children[1].String() + " )"
+}
+
+//////////////////////
+// OpSin
+//////////////////////
+
+type OpSin struct {
+	BaseNode
+}
+
+func NewOpSin() *OpSin {
+	return &OpSin{BaseNode{nil, make([]Node, 1)}}
+}
+
+func (op *OpSin) Eval(x, y float32) float32 {
+	return float32(math.Sin(float64(op.Children[0].Eval(x, y))))
+}
+
+func (op *OpSin) String() string {
+	return "( Sin " + op.Children[0].String() + " )"
+}
+
+//////////////////////
+// OpCos
+//////////////////////
+
+type OpCos struct {
+	BaseNode
+}
+
+func NewOpCos() *OpCos {
+	return &OpCos{BaseNode{nil, make([]Node, 1)}}
+}
+
+func (op *OpCos) Eval(x, y float32) float32 {
+	return float32(math.Cos(float64(op.Children[0].Eval(x, y))))
+}
+
+func (op *OpCos) String() string {
+	return "( Cos " + op.Children[0].String() + " )"
+}
+
+//////////////////////
+// OpAtan
+//////////////////////
+
+type OpAtan struct {
+	BaseNode
+}
+
+func NewOpAtan() *OpAtan {
+	return &OpAtan{BaseNode{nil, make([]Node, 1)}}
+}
+
+func (op *OpAtan) Eval(x, y float32) float32 {
+	return float32(math.Atan(float64(op.Children[0].Eval(x, y))))
+}
+
+func (op *OpAtan) String() string {
+	return "( Atan " + op.Children[0].String() + " )"
+}
+
+//////////////////////
+// OpAtan2
+//////////////////////
+
+type OpAtan2 struct {
+	BaseNode
+}
+
+func NewOpAtan2() *OpAtan2 {
+	return &OpAtan2{BaseNode{nil, make([]Node, 2)}}
+}
+
+func (op *OpAtan2) Eval(x, y float32) float32 {
+	return float32(math.Atan2(float64(op.Children[0].Eval(x, y)), float64(op.Children[1].Eval(x, y))))
+}
+
+func (op *OpAtan2) String() string {
+	return "( Atan2 " + op.Children[0].String() + " " + op.Children[1].String() + " )"
+}
+
+//////////////////////
+// OpNoise
+//////////////////////
+
+type OpNoise struct {
+	BaseNode
+}
+
+func NewOpNoise() *OpNoise {
+	return &OpNoise{BaseNode{nil, make([]Node, 2)}}
+}
+
+func (op *OpNoise) Eval(x, y float32) float32 {
+	// (80*) makes it between 0-2
+	// (- 2.0) maks it between -1 and 1
+	return 80*noise.Snoise2(op.Children[0].Eval(x, y), op.Children[1].Eval(x, y)) - 2.0
+}
+
+func (op *OpNoise) String() string {
+	return "( SimplexNoise " + op.Children[0].String() + " " + op.Children[1].String() + " )"
+}
+
+// Pass any node, make a distinct copy of the entire tree underneath
+func CopyTree(node Node, parent Node) Node {
+	// We can use reflection to ask the type of a struct
+	copy := reflect.New(reflect.ValueOf(node).Elem().Type()).Interface().(Node) // Eg. Make a new node that is an OpPlus. Wrap back up in a node interface. Cast interface as node
+
+	// All constants will be 0, so they won't be all black
+	switch n := node.(type) {
+	case *OpConstant:
+		copy.(*OpConstant).value = n.value
+	}
+
+	// Blank node. No values (parents and children) not filled in.
+	copy.SetParent(parent)
+	copyChildren := make([]Node, len(node.GetChildren())) // Same size as children we are copying
+	copy.SetChildren(copyChildren)
+	for i := range copyChildren {
+		copyChildren[i] = CopyTree(node.GetChildren()[i], copy) // We don't just want to copy the child. Use recursion to get children of children of children...
+	}
+	return copy
+}
+
+// Old node gets replaced by new node
+func ReplaceNode(old Node, new Node) {
+	oldParent := old.GetParent() // Take the old node's parent, and set that to a variable for convenience
+	if oldParent != nil {
+		// Link parent of old node to new node
+		for i, child := range oldParent.GetChildren() {
+			if child == old {
+				oldParent.GetChildren()[i] = new
+			}
+		}
+	}
+	// Wire up old parent to new child
+	new.SetParent(oldParent)
+}
+
+// Get nth random node out of the tree for random mutation
+func GetNthNode(node Node, n, count int) (Node, int) {
+	// We have arrived
+	if n == count {
+		return node, count
+	}
+	var result Node // Set up an empty node
+	// Iterate through the node's children
+	for _, child := range node.GetChildren() {
+		count++
+		result, count = GetNthNode(child, n, count)
+		if result != nil {
+			// We have arrived at the (n)th node
+			return result, count
+		}
+	}
+	return nil, count // We never found the nth node for this step in the iteration
+}
---
+++ 21_parsing/apt/parser.go
@@ -0,0 +1,234 @@
+package apt
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode/utf8"
+)
+
+// Enum
+type tokenType int
+
+const (
+	openParen tokenType = iota // First is 1, next 2, ...
+	closeParen
+	op
+	constant // We have to parse these as numbers, so keep them seperate
+)
+
+type token struct {
+	typ   tokenType // Is it an open paren, number, etc.
+	value string    // Name of operator we found
+}
+
+// Keeps track of where we are in the text
+type lexer struct {
+	input  string     // Entirety of file we are loading in
+	start  int        // Points to start of whatever token we are working on
+	pos    int        // Actual char we are pointed at in the string
+	width  int        // Width of the current character (unicode support)
+	tokens chan token // Have lexer running as it gets tokens!
+}
+
+// Break text into list of things we're interested in (lexing). Ignore tabs, carriage returns, etc.
+// stateFunction takes a pointer to a lexer, returns another state function
+type stateFunc func(*lexer) stateFunc
+
+// Take in a string and return a node
+func stringToNode(s string) Node {
+	switch s {
+	case "+":
+		return NewOpPlus()
+	case "-":
+		return NewOpMinus()
+	case "*":
+		return NewOpMult()
+	case "/":
+		return NewOpDiv()
+	case "Atan2":
+		return NewOpAtan2()
+	case "Atan":
+		return NewOpAtan()
+	case "Cos":
+		return NewOpCos()
+	case "Sin":
+		return NewOpSin()
+	case "SimplexNoise":
+		return NewOpNoise()
+	case "X":
+		return NewOpX()
+	case "Y":
+		return NewOpY()
+	case "Picture":
+		return NewOpPicture()
+	default:
+		panic("Error in stringToNode. Token not understood: " + s)
+	}
+}
+
+// Recursive so we can descend into the tree
+func parse(tokens chan token, parent Node) Node {
+	for {
+		token, ok := <-tokens
+		if !ok {
+			// Check if token channel has been closed
+			fmt.Println("=====", token.value)
+			panic("No more tokens")
+		}
+		//fmt.Print(token.value, ",")
+		// Do something based on what token we get
+		switch token.typ {
+		case op:
+			n := stringToNode(token.value)
+			n.SetParent(parent)
+			for i := range n.GetChildren() {
+				n.GetChildren()[i] = parse(tokens, n) // Go through each child, and set it to the parse of the rest of the tokens
+			}
+			return n // Exit infinite loop
+		case constant:
+			n := NewOpConstant()
+			n.SetParent(parent)
+			// Check for invalid characters
+			v, err := strconv.ParseFloat(token.value, 32) // 32-bit float
+			if err != nil {
+				panic("Error while parsing constant op: " + token.value)
+			}
+			n.value = float32(v)
+			return n // Constants are always leaves, return immediately
+		case closeParen, openParen:
+			continue // We don't even need to know about them. All ops have a known number of children
+		}
+	}
+	return nil // Just lex for now, no parsing
+}
+
+const eof rune = -1
+
+// Returns a node to kick off the parser at the same time
+func BeginLexing(s string) Node {
+	// Make a buffered channel (100). Lexer can keep putting tokens into the channel, even if the parser hasn't pulled them out yet.
+	// (Lexer can keep moving if the parser is lagging behind).
+	l := &lexer{input: s, tokens: make(chan token, 100)} // name:value format = we don't have to provide in order.
+
+	go l.run() // Run lexer on a seperate thread
+
+	return parse(l.tokens, nil) // Parser takes tokens. nil becuase top of treee doesn't have a parent to start recursion
+}
+
+func (l *lexer) run() {
+	// Magic loop to go from one state to another. Set state equal to some type stateFunc.
+	// First statefunction that gets called is determineToken
+	for state := determineToken; state != nil; {
+		state = state(l) // For each loop, state will equal the state it gets back. (for isStartOfNumber)
+	}
+	close(l.tokens) // Close the channel
+}
+
+// State function, start looking for first token to grab
+func determineToken(l *lexer) stateFunc {
+	for {
+		// Grab the next rune (char) in the string. Decide what to do with it
+		switch r := l.next(); {
+		case isWhiteSpace(r):
+			l.ignore() // Ignore it
+		case r == '(':
+			l.emit(openParen) // Ready to spit out a token
+		case r == ')':
+			l.emit(closeParen)
+		case isStartOfNumber(r):
+			return lexNumber // Parse a number. Return statefunc that needs to run next
+		case r == eof:
+			return nil // Close program
+		default:
+			// has to be an Op
+			return lexOp
+		}
+	}
+}
+
+func lexNumber(l *lexer) stateFunc {
+	l.accept("-.")
+	digits := "0123456789"
+	l.acceptRun(digits)
+	// Once we have run out of digits to accept, see if we can accept a period
+	if l.accept(".") {
+		l.acceptRun(digits) // 123.123
+	}
+	// Account for OpMinus (if all we got was a minus)
+	if l.input[l.start:l.pos] == "-" {
+		l.emit(op)
+	} else {
+		l.emit(constant)
+	}
+
+	return determineToken // Go back to determine token state
+}
+
+func lexOp(l *lexer) stateFunc {
+	l.acceptRun("+-/*abcdefhijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123546789") // Match these
+	l.emit(op)
+
+	return determineToken
+}
+
+// lexNumber helper function (1 char)
+func (l *lexer) accept(valid string) bool {
+	// Is the next input one of these things or not
+	if strings.IndexRune(valid, l.next()) >= 0 {
+		return true
+	}
+	l.backup()
+	return false
+}
+
+// accept() for multiple chars
+func (l *lexer) acceptRun(valid string) {
+	// Keep advancing as long as it exists in the string of valid characters
+	for strings.IndexRune(valid, l.next()) >= 0 {
+	}
+	// Then backup
+	l.backup()
+}
+
+func isWhiteSpace(r rune) bool {
+	return r == ' ' || r == '\n' || r == '\t' || r == '\r'
+}
+
+func isStartOfNumber(r rune) bool {
+	return (r >= '0' && r <= '9') || r == '-' || r == '.'
+}
+
+// Rune is a go type that is a "more than a byte" character (unicode)
+func (l *lexer) next() (r rune) {
+
+	if l.pos >= len(l.input) {
+		l.width = 0
+		return eof // We're done with the whole file
+	}
+	r, l.width = utf8.DecodeRuneInString(l.input[l.pos:]) // Get slice from current position of lexor to the end, and width of the next rune
+	l.pos += l.width                                      // Increase position with width of current char
+	return r                                              // Return rune
+}
+
+// If next character is not what we expect, backup
+func (l *lexer) backup() {
+	l.pos -= l.width
+}
+
+// Go further in the input while ignoring (eg. whitespace)
+func (l *lexer) ignore() {
+	l.start = l.pos // Not considering start as part of the token. Just skip it.
+}
+
+// Look at current character without advancing the lexer
+func (l *lexer) peek() (r rune) {
+	r, _ = utf8.DecodeRuneInString(l.input[l.pos:]) // Same decode without updating position
+	return r
+}
+
+// Tell lexer to go ahead and spit out a token
+func (l *lexer) emit(t tokenType) {
+	l.tokens <- token{t, l.input[l.start:l.pos]} // Write to tokens channel from start to pos
+	l.start = l.pos                              // Move on to next token
+}
