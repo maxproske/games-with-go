b25b57698e2ef6a439b6182e016c36e7fc382f3e
Max Proske <mhcproske@gmail.com>
2018-07-08 11:47:20 -0700

    Add event console

---
+++ 30_console/main.go
@@ -0,0 +1,20 @@
+package main
+
+// https://youtu.be/Jy919y3ezOI?t=1346
+
+import (
+	"github.com/maxproske/games-with-go/30_console/game"
+	"github.com/maxproske/games-with-go/30_console/ui2d"
+)
+
+func main() {
+	// Make new game
+	game := game.NewGame(1, "game/maps/level1.map")
+	go func() {
+		game.Run()
+	}()
+
+	// Make our UI
+	ui := ui2d.NewUI(game.InputChan, game.LevelChans[0])
+	ui.Run()
+}
